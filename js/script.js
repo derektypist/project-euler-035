2d8d042682f434bbfd63f795aa6516214e5f3cc4
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-08-04 09:56:24 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -63,3 +63,9 @@ function circularPrimes(n) {
     }
     return count;
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
